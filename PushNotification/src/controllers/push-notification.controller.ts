7325fecb3a6fdd3c785403763de27b4f81a2985c
Mentlejain <arnavj99@gmail.com>
2023-12-14 01:14:54 +0530

    Added batch send API

--- PushNotification/src/controllers/push-notification.controller.ts
+++ PushNotification/src/controllers/push-notification.controller.ts
@@ -101,6 +101,133 @@ export class PushNotificationController {
         response,
       };
     }
+    @post('/register-group')
+    @response(200, {
+      description: 'Register group',
+      content: {
+        'application/json': {
+          schema: {
+            type: 'object',
+            title: 'RegisterGroupResponse',
+            properties: {
+              success: { type: 'boolean' },
+              response: { type: 'object' },
+            },
+          },
+        },
+      },
+    })
+    async registerGroup(
+      @requestBody({
+        content: {
+          'application/json': {
+            schema: {
+              type: 'object',
+              title: 'RegisterGroupRequest',
+              properties: {
+                groupName: { type: 'string' },
+                tokens: { type: 'string' },
+              },
+            },
+            required: ['groupName', 'tokens'],
+          },
+        },
+      })
+      group: any,
+    ): Promise<any> {
+      // Use the FCM SDK to register a device group
+      const messaging = this.firebaseAdmin.messaging();
+  
+      const response = await messaging
+        .subscribeToTopic(group.tokens, group.groupName)
+        .catch(error => {
+          console.error('Failed to register group:', error);
+          return error;
+        });
+  
+      return {
+        success: true,
+        response,
+      };
+    }
+
+    @post('/push-notifications')
+    @response(200, {
+      description: 'Send push notifications',
+      content: {
+        'application/json': {
+          schema: {
+            type: 'object',
+            title: 'PushNotificationsResponse',
+            properties: {
+              success: { type: 'boolean' },
+              response: { type: 'object' },
+            },
+          },
+        },
+      },
+    })
+    async sendPushNotifications(
+      @requestBody({
+        content: {
+          'application/json': {
+            schema: {
+              type: 'object',
+              title: 'PushNotificationsRequest',
+              properties: {
+                groupName: { type: 'string' },
+                title: { type: 'string' },
+                body: { type: 'string' },
+                imageUrl: { type: 'string' },
+              },
+            },
+            required: ['groupName', 'title', 'body'],
+          },
+        },
+      })
+      notification: any,
+    ): Promise<any> {
+      // Use the FCM SDK to send push notifications
+      const messaging = this.firebaseAdmin.messaging();
+//retriece the tokens from token.csv and store in registrationTokens
+      const tokenData = JSON.parse(fs.readFileSync("./src/repositories/token.json").toString())
+      
+      const registrationTokens = tokenData.map(
+        (entry:FcmTokenData) => entry.fcmToken
+      );
+  
+      const message = {
+        notification: {
+          title: notification.title,
+          body: notification.body,
+          imageUrl: notification.imageUrl,
+        },
+        tokens: registrationTokens ,
+      };
+  
+      const options: admin.messaging.MessagingOptions = {
+        priority: 'high',
+      };
+  
+      messaging.sendEachForMulticast(message)
+  .then((response) => {
+    if (response.failureCount > 0) {
+      const failedTokens: string | any[] = [];
+      response.responses.forEach((resp, idx) => {
+        if (!resp.success) {
+          failedTokens.push(registrationTokens[idx]);
+        }
+      });
+      console.log('List of tokens that caused failures: ' + failedTokens);
+    }
+  });
+  
+      return {
+        success: true,
+        response,
+      };
+    }
+
     @post('/broadcast-push-notification')
     @response(200, {
       description: 'Broadcast push notification',
